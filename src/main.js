92dfdebbeebbdadd63170eb13387b9b7f7e7eff0
GeeHsu <james81429@yahoo.com.tw>
2023-08-21 16:38:44 +0800

    add ga

--- src/main.js
+++ src/main.js
@@ -7,11 +7,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createHead } from "@vueuse/head"
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 const head = createHead()
 
 app.use(head)
+
+app.use(VueGtag, {
+  config: { id: "G-K8TH1VX1V2" }
+})
 
 app.use(router)
 
