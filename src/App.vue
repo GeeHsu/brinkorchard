9686b0a3e2766ff226607b510fdb8c60d23bc520
GeeHsu <james81429@yahoo.com.tw>
2023-08-25 15:22:13 +0800

    update meta

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@ import { RouterLink, RouterView } from 'vue-router'
 import PageLoader from './components/front/PageLoader.vue'
 
 // Right Click Disabled
-// window.addEventListener('contextmenu', function (e) { 
-//   // do something here... 
-//   e.preventDefault(); 
-// }, false);
+window.addEventListener('contextmenu', function (e) { 
+  // do something here... 
+  e.preventDefault(); 
+}, false);
 </script>
 
 <style scoped>
